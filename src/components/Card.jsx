d0a7f0ba59125b7fcb687ca1aa40a7ea2fc0d30f
Jesus Arroyo <jose.olalde@alumnos.udg.mx>
2024-01-30 15:37:47 -0600

    Update component card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,12 @@
 const Card = ({card}) => {
     return ( 
-        <div className="">
+        <>
             <img 
                 src={card.imageUrl} 
                 alt={card.name} 
-                className="w-full rounded-xl hover:scale-110 transition shadow-md shadow-slate-500/50" 
+                className="h-64 rounded-xl hover:scale-95 transition shadow-md shadow-slate-500/50" 
             />
-                
-            <div className="p-4 flex items-center justify-between">
-                <h2 className="text-base font-medium">
-                    {card.name}
-                </h2>
-                <h2 className="text-base font-medium">
-                    {card.set}
-                </h2>
-            </div>
-        </div>
+        </>
      );
 }
 

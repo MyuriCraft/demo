f9e66815ef83ef1b7d699df93d138c57d4dc8a50
Jesus Arroyo <jose.olalde@alumnos.udg.mx>
2024-01-31 17:57:42 -0600

    Add toast when add to card

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,9 @@ import { Drawer, DrawerClose, DrawerContent, DrawerDescription, DrawerFooter, Dr
 import { Button } from './components/ui/button';
 import Card from './components/Card';
 
+import { toast } from "sonner";
+import { Toaster } from "@/components/ui/sonner";
+
 function App() {
   const [id_sesion, set_id_sesion] = useState(uniqid())
   const [mano, set_mano] = useState([])
@@ -48,7 +51,22 @@ function App() {
     const sec = Date.now() * 1000 + Math.random() * 1000;
     const id = sec.toString(16).replace(/\./g, "").padEnd(14, "0");
     return `${prefix}${id}${random ? `.${Math.trunc(Math.random() * 100000000)}`:""}`;
-}
+  }
+  const handleClick = () => {
+    if(deck_cartas.length > 0){
+      var aux_mano = JSON.parse(JSON.stringify(mano));
+      var aux_deck = JSON.parse(JSON.stringify(deck_cartas));
+
+      aux_mano.push(aux_deck[0])
+      set_mano(aux_mano)
+
+      aux_deck.splice(0, 1)
+      set_deck_cartas(aux_deck)
+      toast.success("Carta agregada a tu mano")
+    }else{
+      toast.warning("Sin cartas")
+    }
+  }
 
   function render_mano(){
     var datos = Object.values(mano_enemigo);
@@ -258,7 +276,7 @@ function App() {
 
   return(
     <section className="h-dvh bg-slate-100 flex flex-col">
-      <div className="basis-1/3 bg-slate-300">
+      <div className="basis-1/3 bg-slate-300 h-2/6">
         {campo_criaturas.map((card, i) => 
           <Card 
             key={i} 
@@ -284,7 +302,7 @@ function App() {
           />
         )}
       </div>
-      <div className="basis-1/3 bg-slate-200">
+      <div className="basis-1/3 bg-slate-200 h-2/6">
         {campo_tierras.map((card, i) => 
           <Card 
             key={i} 
@@ -310,66 +328,66 @@ function App() {
           />
         )}
       </div>
-      <div className="basis-1/3">
+      <div className="basis-1/3 flex flex-row-reverse h-2/6">
+        <Toaster position="top-right" />
+        <button onClick={handleClick} >
+          <img 
+          src="https://m.media-amazon.com/images/I/61AGZ37D7eL.jpg"
+          alt="Deck Magic" 
+          className='h-full rounded-xl'
+          />
+        </button>
         <Drawer>
           <DrawerTrigger>
-            <Button variant="outline">Hand</Button>
+            <Button variant="outline" className="absolute bottom-0 left-0">Ver mano</Button>
           </DrawerTrigger>
           <DrawerContent>
             <DrawerHeader>
               <DrawerDescription className="flex gap-4 overflow-x-auto">
-                <div
-                  onClick={() => {
-                    if(deck_cartas.length > 0){
-                      var aux_mano = JSON.parse(JSON.stringify(mano));
-                      var aux_deck = JSON.parse(JSON.stringify(deck_cartas));
-
-                      aux_mano.push(aux_deck[0])
-                      set_mano(aux_mano)
-
-                      aux_deck.splice(0, 1)
-                      set_deck_cartas(aux_deck)
-                    }else{
-                      console.log(deck_cartas.length)
-                    }
-                  }}
-                >
-                  Deck
-                </div>
-                {mano.map((card, i) => 
-                  <Card 
-                    key={i} 
-                    card={{
-                      name: card,
-                      imageUrl: cartas[card]['img']
-                    }} 
-                    inv={() => {
-                      var aux_mano = JSON.parse(JSON.stringify(mano)); 
-                      var campo = null;
-                      if(cartas[aux_mano[i]]['tipo'] === 'tierra'){
-                        campo = JSON.parse(JSON.stringify(campo_tierras)); 
-                        campo.push(aux_mano[i]);
-                        set_campo_tierras(campo)
-                      }else{
-                        campo = JSON.parse(JSON.stringify(campo_criaturas)); 
-                        campo.push(aux_mano[i]);
-                        set_campo_criaturas(campo)
-                      }
-                      aux_mano.splice(i, 1);
-                      set_mano(aux_mano)
-                    }}
-                  />
-                )}
+                { 
+                  mano.map((card, i) => 
+                    <Card 
+                      key={i} 
+                      card={{
+                        name: card,
+                        imageUrl: cartas[card]['img']
+                      }} 
+                      inv={() => {
+                        var aux_mano = JSON.parse(JSON.stringify(mano)); 
+                        var campo = null;
+                        if(cartas[aux_mano[i]]['tipo'] === 'tierra'){
+                          campo = JSON.parse(JSON.stringify(campo_tierras)); 
+                          campo.push(aux_mano[i]);
+                          set_campo_tierras(campo)
+                        }else{
+                          campo = JSON.parse(JSON.stringify(campo_criaturas)); 
+                          campo.push(aux_mano[i]);
+                          set_campo_criaturas(campo)
+                        }
+                        aux_mano.splice(i, 1);
+                        set_mano(aux_mano)
+                      }}
+                    />
+                  )
+                }
+                {mano.length == "0" ? 
+                  <img src="https://media.newyorker.com/photos/62d85d3d3b3e14a2fb1cf46e/16:9/w_1280,c_limit/site_deRecat_hand.jpg" 
+                  className='h-64 object-cover w-50'/>
+                : null }
               </DrawerDescription>
             </DrawerHeader>
-            <DrawerFooter>
+            <DrawerFooter className="flex flex-row flex-row-reverse gap-4">
+              <Button onClick={handleClick} >
+                Robar carta
+              </Button>
               <DrawerClose>
-                <Button variant="outline">Cerrar</Button>
+                <Button variant="outline">Ocultar</Button>
               </DrawerClose>
             </DrawerFooter>
           </DrawerContent>
         </Drawer>
       </div>
+
     </section>
   )
 

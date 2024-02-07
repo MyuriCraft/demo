badad8d55cc1b127df5596d2603308026e2a2c18
Jesus Arroyo <jose.olalde@alumnos.udg.mx>
2024-02-07 15:41:06 -0600

    update(card): add click menu on cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,6 +3,11 @@ import {
 	ContextMenuContent,
 	ContextMenuItem,
 	ContextMenuTrigger,
+	ContextMenuSeparator,
+	ContextMenuSub,
+	ContextMenuSubContent,
+	ContextMenuSubTrigger,
+	ContextMenuShortcut,
 } from "@/components/ui/context-menu";
 import propTypes from 'prop-types';
 import { useEffect } from "react";
@@ -32,7 +37,7 @@ const Card = ({ card, func_card_state, inv, place, ret_deck }) => {
 		console.log(card)
 	}, [])
 
-	return( 
+	return card['image_uris']['png'] !== undefined ? ( 
 		<ContextMenu>
 			<ContextMenuTrigger asChild className="h-full max-h-64 rounded-xl hover:scale-95 transition" >
 					<img 
@@ -45,43 +50,81 @@ const Card = ({ card, func_card_state, inv, place, ret_deck }) => {
 								tipo: get_type()
 							})
 						}
-						src={card['cubierta'] ? "https://m.media-amazon.com/images/I/61AGZ37D7eL.jpg":card['image_uris']['png']} 
+						src={card['cubierta'] ? "https://m.media-amazon.com/images/I/61AGZ37D7eL.jpg":card['image_uris']['normal']} 
 						alt={card['name'] ?? ''} 
 					/>
 			</ContextMenuTrigger>
-			<ContextMenuContent>
+			<ContextMenuContent className="w-64">
 				{place === 'hand' &&
 					<>
-						<ContextMenuItem
+						<ContextMenuItem inset
 							onClick={() => inv()}
 						>
-							Añadir al campo
+							Castear
 						</ContextMenuItem>
-						<ContextMenuItem
-							onClick={() => ret_deck()}
-						>
-							devolver al deck
-						</ContextMenuItem>
+						
 					</>
 				}	
-				{func_card_state !== undefined &&
-					<ContextMenuItem
-						onClick={() => 
-							func_card_state({
-								action: 'hide',
-								tipo: get_type()
-							})
-						}
-					>
-						{card['cubierta'] ? 'Mostrar':'Ocultar'}
-					</ContextMenuItem>
+				{ func_card_state !== undefined &&
+					<>
+						<ContextMenuItem inset>
+							Tappear
+						</ContextMenuItem>
+						<ContextMenuItem inset>
+							Devolver a la mano
+						</ContextMenuItem>
+						<ContextMenuSeparator />
+						<ContextMenuItem inset
+							onClick={() => 
+								func_card_state({
+									action: 'hide',
+									tipo: get_type()
+								})
+							}
+							>
+							{card['cubierta'] ? 'Mostrar':'Ocultar'}
+						</ContextMenuItem>
+					</>
 				}
-				{/*<ContextMenuItem>Billing</ContextMenuItem>
 				<ContextMenuSeparator />
-				<ContextMenuItem>Team</ContextMenuItem>
-	<ContextMenuItem>Subscription</ContextMenuItem>*/}
+				<ContextMenuItem inset>
+					Descartar
+				</ContextMenuItem>
+				<ContextMenuItem inset>
+					Exiliar
+				</ContextMenuItem>
+				<ContextMenuSeparator />
+				<ContextMenuSub>
+					<ContextMenuSubTrigger inset>Devolver a la librería</ContextMenuSubTrigger>
+					<ContextMenuSubContent className="w-48">
+						<ContextMenuItem inset onClick={() => ret_deck()}>
+							Barajar en librería
+							{/* <ContextMenuShortcut>⇧⌘S</ContextMenuShortcut> */}
+						</ContextMenuItem>
+						<ContextMenuItem inset disabled>Enviar al fondo</ContextMenuItem>
+						<ContextMenuItem inset>Enviar al tope</ContextMenuItem>
+						{place === 'hand' &&
+							<>
+								<ContextMenuSeparator />
+								<ContextMenuItem inset>Revelar</ContextMenuItem>
+							</>
+						}
+					</ContextMenuSubContent>
+				</ContextMenuSub>
+				{ func_card_state !== undefined &&
+					<>
+						<ContextMenuSeparator />
+						<ContextMenuItem inset>
+							Agregar Palabra Clave
+						</ContextMenuItem>
+					</>
+				}
 			</ContextMenuContent>
 		</ContextMenu>
+	):(
+		<div>
+			no deberias poder ver esto
+		</div>
 	);
 }
 
